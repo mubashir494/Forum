f0b19588bb513bfd1d2633988eda3586f18c52a1
Muhammad Mubashir <mubashir@example.com>
2022-06-04 18:09:14 +0500

    minor changes

--- index.js
+++ index.js
@@ -23,9 +23,13 @@ app.use('/user',user)
 const CONNECTION_URL =  process.env.MONGODB_URI || 'mongodb+srv://server:MbIWrHCmhUYXO78b@cluster0.gdrhq.mongodb.net/secondDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT|| 5000
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser : true, useUnifiedTopology : true})
-.then(() => {app.listen(PORT,() => console.log("Connected to port "+PORT))}).catch((error) => {console.log(error.message)})
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
+    const path = require("path")
+    app.get("*",(req,res) =>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
 }
+mongoose.connect(CONNECTION_URL,{useNewUrlParser : true, useUnifiedTopology : true})
+.then(() => {app.listen(PORT,() => console.log("Connected to port "+PORT))}).catch((error) => {console.log(error.message)})
 // mongoose.set('useFindAndModify',true);
