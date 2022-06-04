f23f2b8789a80b9f89cf80a728a4ec2d069897d6
Muhammad Mubashir <mubashir@example.com>
2022-06-04 18:13:18 +0500

    changes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import post from './route/post.js'
 import comment from './route/comment.js'
 import user from './route/user.js'
 import dotenv from 'dotenv'
+import path from 'path'
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,7 +26,6 @@ const PORT = process.env.PORT|| 5000
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
-    const path = require("path")
     app.get("*",(req,res) =>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
