2821423ae44f6b34aa22a6012e22dddf694bcf82
Muhammad Mubashir <mubashir@example.com>
2022-06-04 19:19:44 +0500

    Updating the urls

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,32 +1,35 @@
 import axios from 'axios'
 
-export const createPost = async (obj) => { return axios.post('http://localhost:5000/post', obj)};
+
+const URL = "https://forum-q.herokuapp.com"
+
+export const createPost = async (obj) => { return axios.post(`${URL}/post`, obj)};
 
 export const getPost = async () => {
-    return axios.get('http://localhost:5000/post').then(response => response.data);
+    return axios.get(`${URL}/post`).then(response => response.data);
 }
 export const getPostById = async (id) => {
-    const url = `http://localhost:5000/post/id?id=${id}`
+    const url = `${URL}/post/id?id=${id}`
     return axios.get(url).then(response => response.data);
 
 }
 export const createComment  = async (obj) => {
-  axios.post('http://localhost:5000/comment', obj)
+  axios.post(`${URL}/comment`, obj)
 }
 
 export const getCommentById = async (id) => {
-  return axios.get(`http://localhost:5000/comment?id=${id}`)
+  return axios.get(`${URL}/comment?id=${id}`)
 } 
 
 export const registerUser = async (userObj) =>{
-  return axios.post('http://localhost:5000/user/register',userObj)
+  return axios.post(`${URL}/user/register`,userObj)
 }
 export const login = (userObj) =>{
-  return axios.post('http://localhost:5000/user/',userObj)
+  return axios.post(`${URL}/user/`,userObj)
 } 
 
 export const isLoggedIn = (token) => {
-  return axios.post('http://localhost:5000/user/get',null,{
+  return axios.post(`${URL}/user/get`,null,{
     headers : {
       'Content-Type' : 'application/json',
       'token' : token
@@ -35,14 +38,14 @@ export const isLoggedIn = (token) => {
 }
 
 export const getPostByUserId = (userid) =>{
-  return axios.get(`http://localhost:5000/post/getpost?userId=${userid}`)
+  return axios.get(`${URL}/post/getpost?userId=${userid}`)
 
 }
 export const getPostbyUser = (userid) =>{
-  return axios.get(`http://localhost:5000/post/getPostbyUserId?userId=${userid}`)
+  return axios.get(`${URL}/post/getPostbyUserId?userId=${userid}`)
 } 
 export const updateUser = (token,obj) =>{
-  return axios.put('http://localhost:5000/user/update',obj,{
+  return axios.put(`${URL}/user/update`,obj,{
     headers : {
       'Content-Type' : 'application/json',
       'token' : token
@@ -50,24 +53,24 @@ export const updateUser = (token,obj) =>{
   })
 }
 export const delPost = (id) =>{
-  return axios.delete(`http://localhost:5000/post/deletePost?postId=${id}`)
+  return axios.delete(`${URL}/post/deletePost?postId=${id}`)
 }
 export const delComment = (id) =>{
-  return axios.delete(`http://localhost:5000/comment/del?commentId=${id}`)
+  return axios.delete(`${URL}/comment/del?commentId=${id}`)
 }
 export const like = (postId,userId)=>{
-  return axios.put(`http://localhost:5000/post/like?userId=${userId}&postId=${postId}`)
+  return axios.put(`${URL}/post/like?userId=${userId}&postId=${postId}`)
 }
 export const dislike = (postId,userId)=>{
-  return axios.put(`http://localhost:5000/post/dislike?userId=${userId}&postId=${postId}`)
+  return axios.put(`${URL}/post/dislike?userId=${userId}&postId=${postId}`)
 }
 
 export const likeComment =(commentId,userId) =>{
-  return axios.put(`http://localhost:5000/comment/like?userId=${userId}&commentId=${commentId}`)
+  return axios.put(`${URL}/comment/like?userId=${userId}&commentId=${commentId}`)
 }
 export const dislikeComment = (commentId,userId) =>{
-  return axios.put(`http://localhost:5000/comment/dislike?userId=${userId}&commentId=${commentId}`)
+  return axios.put(`${URL}/comment/dislike?userId=${userId}&commentId=${commentId}`)
 }
 export const postLikedByUser = (userId) =>{
-  return axios.get(`http://localhost:5000/post/getlikedPost?userId=${userId}`)
+  return axios.get(`${URL}/post/getlikedPost?userId=${userId}`)
 }
